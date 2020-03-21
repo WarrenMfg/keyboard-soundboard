d5fec80deb33636c56ec796cc399364b4b4948c1
WarrenMfg <warrenmfg@icloud.com>
2020-03-21 13:56:04 -0400

    added static routes; moving to CSS

--- server/index.js
+++ server/index.js
@@ -6,16 +6,31 @@ const morgan = require('morgan');
 
 const app = express();
 app.use(morgan('dev'));
+app.use(express.json());
 
 // API
 
 
 
 // STATIC
-app.get('/bundle.js', (req, res) => {
-  const html = fs.createReadStream(path.resolve(__dirname, '../client/bundle.js'));
+app.use('/favicon', express.static(path.resolve(__dirname, '../favicon')));
+
+app.get('/vinyl.jpg', (req, res) => {
+  const image = fs.createReadStream(path.resolve(__dirname, '../client/vinyl.jpg'));
   res.set({ 'Content-Encoding': 'gzip' });
-  html.pipe(zlib.createGzip()).pipe(res);
+  image.pipe(zlib.createGzip()).pipe(res);
+});
+
+app.get('/sounds/*', (req, res) => {
+  const sound = req.url.split('/')[2];
+  const file = fs.createReadStream(path.resolve(__dirname, `../sounds/${sound}`));
+  file.pipe(res);
+});
+
+app.get('/bundle.js', (req, res) => {
+  const bundle = fs.createReadStream(path.resolve(__dirname, '../client/bundle.js'));
+  res.set({ 'Content-Encoding': 'gzip' });
+  bundle.pipe(zlib.createGzip()).pipe(res);
 });
 
 app.get('/', (req, res) => {
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,72 @@
 import React from 'react';
+import '../style.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown(e) {
+    // for CSS
+    const div = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    div.classList.add('playing');
+    
+    // for Audio
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if (!audio) {
+      return;
+    }
+    audio.currentTime = 0;
+    audio.play();
+
+    // add transitionend eventListener
   }
 
   render() {
     return (
-      <div>Hello world!</div>
+      <div className="keys">
+        <div data-key="65" className="key">
+          <kbd>A</kbd>
+          <span className="sound">clap</span>
+        </div>
+        <div data-key="83" className="key">
+          <kbd>S</kbd>
+          <span className="sound">hihat</span>
+        </div>
+        <div data-key="68" className="key">
+          <kbd>D</kbd>
+          <span className="sound">kick</span>
+        </div>
+        <div data-key="70" className="key">
+          <kbd>F</kbd>
+          <span className="sound">openhat</span>
+        </div>
+        <div data-key="71" className="key">
+          <kbd>G</kbd>
+          <span className="sound">boom</span>
+        </div>
+        <div data-key="72" className="key">
+          <kbd>H</kbd>
+          <span className="sound">ride</span>
+        </div>
+        <div data-key="74" className="key">
+          <kbd>J</kbd>
+          <span className="sound">snare</span>
+        </div>
+        <div data-key="75" className="key">
+          <kbd>K</kbd>
+          <span className="sound">tom</span>
+        </div>
+        <div data-key="76" className="key">
+          <kbd>L</kbd>
+          <span className="sound">tink</span>
+        </div>
+      </div>
     );
   }
 }
